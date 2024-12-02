96d145abc1f00d7633c1623ecc51d8b1d4f78bb4
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-12-02 12:11:45 +0100

    initial

---
+++ src/main/kotlin/com/example/oneclickbooking/OneClickBookingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.oneclickbooking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OneClickBookingApplication
+
+fun main(args: Array<String>) {
+    runApplication<OneClickBookingApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/oneclickbooking/OneClickBookingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.oneclickbooking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OneClickBookingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
