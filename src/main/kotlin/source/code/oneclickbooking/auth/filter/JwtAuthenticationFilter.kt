e6d37080fd0d08153a79fc23eba6e9efea988b9b
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-12-04 17:26:10 +0100

    security setup classes finished

--- src/main/kotlin/source/code/oneclickbooking/auth/filter/JwtAuthenticationFilter.kt
+++ src/main/kotlin/source/code/oneclickbooking/auth/filter/JwtAuthenticationFilter.kt
@@ -1,4 +1,66 @@
 package source.code.oneclickbooking.auth.filter
 
-class JwtAuthenticationFilter {
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpFilter
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.authentication.AuthenticationFailureHandler
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
+import source.code.oneclickbooking.auth.JwtAuthenticationSuccessHandler
+import source.code.oneclickbooking.auth.JwtService
+import source.code.oneclickbooking.service.declaration.UserService
+
+class JwtAuthenticationFilter(
+    private val authenticationManager: AuthenticationManager,
+    jwtService: JwtService,
+    userService: UserService
+) : HttpFilter() {
+    private val defaultRequestMatcher: RequestMatcher =
+        AntPathRequestMatcher("/api/users/login", "POST")
+
+    private val failureHandler: AuthenticationFailureHandler =
+        SimpleUrlAuthenticationFailureHandler()
+
+    private val successHandler: AuthenticationSuccessHandler =
+        JwtAuthenticationSuccessHandler(jwtService, userService)
+
+    override fun doFilter(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        chain: FilterChain
+    ) {
+        if (!defaultRequestMatcher.matches(request)) {
+           chain.doFilter(request, response)
+           return
+        }
+
+        try {
+            val jwtAuthentication = ObjectMapper().readValue(
+                request.inputStream,
+                JwtAuthenticationToken::class.java
+            )
+
+            val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(
+                jwtAuthentication.username,
+                jwtAuthentication.password
+            )
+
+            val authenticationResult: Authentication = authenticationManager
+                .authenticate(usernamePasswordAuthenticationToken)
+
+            successHandler.onAuthenticationSuccess(request, response, authenticationResult)
+        } catch (e: AuthenticationException) {
+            failureHandler.onAuthenticationFailure(request, response, e)
+        }
+    }
+
+    private data class JwtAuthenticationToken(val username: String, val password: String)
 }
