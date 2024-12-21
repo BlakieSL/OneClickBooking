337312086eb05e348423d7aa9ca14a4c6d4e7684
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-12-21 16:33:47 +0100

    full test coverage for booking validation

--- src/test/kotlin/source/code/oneclickbooking/integration/controller/BookingControllerTest.kt
+++ src/test/kotlin/source/code/oneclickbooking/integration/controller/BookingControllerTest.kt
@@ -20,6 +20,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.testcontainers.junit.jupiter.Testcontainers
 import source.code.oneclickbooking.auth.CustomAuthenticationToken
 import source.code.oneclickbooking.integration.annotation.SqlSetup
+import kotlin.math.log
 
 @ActiveProfiles("test")
 @Testcontainers
@@ -45,7 +46,7 @@ class BookingControllerTest {
     @DisplayName("Test GET /api/bookings/{id} should return booking")
     @SqlSetup
     fun `test get should return booking`() {
-        LOGGER.info("RUNNING test get booking by id should return booking...")
+        logRunning()
 
         val bookingId = 1
 
@@ -58,7 +59,7 @@ class BookingControllerTest {
             .andExpect(jsonPath("$.employeeId").value(1))
             .andExpect(jsonPath("$.treatmentId").value(1))
 
-        LOGGER.info("test get booking by id should return booking PASSED!")
+        logPassed()
     }
 
     @Test
@@ -66,25 +67,26 @@ class BookingControllerTest {
     @DisplayName("Test GET /api/bookings/{id} should return 404 when booking not found")
     @SqlSetup
     fun `test get should return 404 when booking not found`() {
-        LOGGER.info("RUNNING test get booking by id should return 404 when booking not found...")
+        logRunning()
+
         val bookingId = 100
 
         mockMvc.perform(get("/api/bookings/$bookingId"))
             .andExpect(status().isNotFound)
 
-        LOGGER.info("test get booking by id should return 404 when booking not found PASSED!")
+        logPassed()
     }
 
     @Test
     @WithMockUser(username = "testuser", roles = ["USER"])
     @DisplayName("Test GET /api/bookings/{id} should return 400 when id not a number")
     fun `test get should return 400 when id not a number`() {
-        LOGGER.info("RUNNING test get booking by id should return 400 when id not a number...")
+        logRunning()
 
         mockMvc.perform(get("/api/bookings/abc"))
             .andExpect(status().isBadRequest)
 
-        LOGGER.info("test get booking by id should return 400 when id not a number PASSED!")
+        logPassed()
     }
 
     @Test
@@ -92,7 +94,7 @@ class BookingControllerTest {
     @DisplayName("Test GET /api/bookings should return all bookings")
     @SqlSetup
     fun `test get all should return all bookings`() {
-        LOGGER.info("RUNNING test get all bookings should return all bookings...")
+        logRunning()
 
         mockMvc.perform(get("/api/bookings"))
             .andExpect(status().isOk)
@@ -104,20 +106,20 @@ class BookingControllerTest {
             .andExpect(jsonPath("$[0].employeeId").value(1))
             .andExpect(jsonPath("$[0].treatmentId").value(1))
 
-        LOGGER.info("test get all bookings should return all bookings PASSED!")
+        logPassed()
     }
 
     @Test
     @WithMockUser(username = "testuser", roles = ["USER"])
     @DisplayName("Test GET /api/bookings should return empty list when no bookings")
     fun `test get all should return empty list when no bookings`() {
-        LOGGER.info("RUNNING test get all bookings should return empty list when no bookings...")
+        logRunning()
 
         mockMvc.perform(get("/api/bookings"))
             .andExpect(status().isOk)
             .andExpect(jsonPath("$.size()").value(0))
 
-        LOGGER.info("test get all bookings should return empty list when no bookings PASSED!")
+        logPassed()
     }
 
     @Test
@@ -125,7 +127,7 @@ class BookingControllerTest {
     @DisplayName("Test POST /api/bookings should create booking")
     @SqlSetup
     fun `test create should create booking`() {
-        LOGGER.info("RUNNING test create booking should create booking...")
+        logRunning()
 
         val requestBody = """
             {
@@ -149,7 +151,7 @@ class BookingControllerTest {
             .andExpect(jsonPath("$.employeeId").value(1))
             .andExpect(jsonPath("$.treatmentId").value(1))
 
-        LOGGER.info("test create booking should create booking PASSED!")
+        logPassed()
     }
 
     @Test
@@ -158,8 +160,7 @@ class BookingControllerTest {
             "have bookings on the selected date")
     @SqlSetup
     fun `test create should create booking when employee has bookings`() {
-        LOGGER.info("RUNNING test create booking should create booking, when selected employee " +
-                "have bookings on the selected date...")
+        logRunning()
 
         val requestBody = """
             {
@@ -183,7 +184,7 @@ class BookingControllerTest {
             .andExpect(jsonPath("$.employeeId").value(1))
             .andExpect(jsonPath("$.treatmentId").value(1))
 
-        LOGGER.info("test create should create booking when employee has bookings PASSED!")
+        logPassed()
     }
 
     @Test
@@ -192,8 +193,7 @@ class BookingControllerTest {
             "have splitted availability on selected date")
     @SqlSetup
     fun `test create should create booking when employee has splitted availability`() {
-        LOGGER.info("RUNNING test create booking should create booking, when selected employee " +
-                "have splitted availability on selected date...")
+        logRunning()
 
         val requestBody = """
             {
@@ -217,7 +217,7 @@ class BookingControllerTest {
             .andExpect(jsonPath("$.employeeId").value(2))
             .andExpect(jsonPath("$.treatmentId").value(2))
 
-        LOGGER.info("test create should create booking when employee has splitted availability PASSED!")
+        logPassed()
     }
 
     @Test
@@ -236,8 +236,7 @@ class BookingControllerTest {
         )
     )
     fun `test create should create booking when employee has splitted availability and bookings`() {
-        LOGGER.info("RUNNING test create booking should create booking, when selected employee " +
-                "have splitted availability on selected date and bookings...")
+        logRunning()
 
         val requestBody = """
             {
@@ -261,8 +260,7 @@ class BookingControllerTest {
             .andExpect(jsonPath("$.employeeId").value(2))
             .andExpect(jsonPath("$.treatmentId").value(2))
 
-        LOGGER.info("test create should create booking when employee has splitted availability" +
-                " and bookings PASSED!")
+        logPassed()
     }
 
     @Test
@@ -270,7 +268,25 @@ class BookingControllerTest {
     @DisplayName("Test POST /api/bookings should return 400 when employee does not provide treatment")
     @SqlSetup
     fun `test create should return 400 when employee does not provide treatment`() {
+        logRunning()
 
+        val requestBody = """
+            {
+                "date": "2025-01-02T10:00:00",
+                "userId": 1,
+                "servicePointId": 1,
+                "employeeId": 1,
+                "treatmentId": 3
+            }
+        """.trimIndent()
+
+        mockMvc.perform(
+            post("/api/bookings")
+                .contentType("application/json")
+                .content(requestBody)
+        ).andExpect(status().isBadRequest)
+
+        logPassed()
     }
 
     @Test
@@ -279,7 +295,25 @@ class BookingControllerTest {
             "with service point")
     @SqlSetup
     fun `test create should return 400 when employee is not associated with service point`() {
+        logRunning()
 
+        val requestBody = """
+            {
+                "date": "2025-01-02T10:00:00",
+                "userId": 1,
+                "servicePointId": 2,
+                "employeeId": 1,
+                "treatmentId": 1
+            }
+        """.trimIndent()
+
+        mockMvc.perform(
+            post("/api/bookings")
+                .contentType("application/json")
+                .content(requestBody)
+        ).andExpect(status().isBadRequest)
+
+        logPassed()
     }
 
     @Test
@@ -288,23 +322,86 @@ class BookingControllerTest {
             "at the specified date")
     @SqlSetup
     fun `test create should return 400 when employee is not available at the specified date`() {
+        logRunning()
 
+        val requestBody = """
+            {
+                "date": "2025-01-05T15:15:00",
+                "userId": 1,
+                "servicePointId": 1,
+                "employeeId": 2,
+                "treatmentId": 2
+            }
+        """.trimIndent()
+
+        mockMvc.perform(
+            post("/api/bookings")
+                .contentType("application/json")
+                .content(requestBody)
+        ).andExpect(status().isBadRequest)
+
+        logPassed()
     }
 
     @Test
     @WithMockUser(username = "testuser", roles = ["USER"])
     @DisplayName("Test POST /api/bookings should return 400 when employee is not available " +
-            "at specified time slot")
+            "at specified time slot due to different availability")
     @SqlSetup
     fun `test create should return 400 when employee is not available at specified time slot`() {
+        logRunning()
 
+        val requestBody = """
+            {
+                "date": "2025-01-06T14:00:00",
+                "userId": 1,
+                "servicePointId": 1,
+                "employeeId": 2,
+                "treatmentId": 2
+            }
+        """.trimIndent()
+
+        mockMvc.perform(
+            post("/api/bookings")
+                .contentType("application/json")
+                .content(requestBody)
+        ).andExpect(status().isBadRequest)
+
+        logPassed()
+    }
+
+    @Test
+    @WithMockUser(username = "testuser", roles = ["USER"])
+    @DisplayName("Test POST /api/bookings should return 400 when employee is not available " +
+            "at specified time slot due bookings")
+    @SqlSetup
+    fun `test create should return 400 when employee is not available at specified time slot due bookings`() {
+        logRunning()
+
+        val requestBody = """
+            {
+                "date": "2025-01-06T15:00:00",
+                "userId": 1,
+                "servicePointId": 1,
+                "employeeId": 1,
+                "treatmentId": 1
+            }
+        """.trimIndent()
+
+        mockMvc.perform(
+            post("/api/bookings")
+                .contentType("application/json")
+                .content(requestBody)
+        ).andExpect(status().isBadRequest)
+
+        logPassed()
     }
 
     @Test
     @WithMockUser(username = "testuser", roles = ["USER"])
     @DisplayName("Test POST /api/bookings should return 400 when date is in the past")
     fun `test create should return 400 when date is in the past`() {
-        LOGGER.info("RUNNING test create booking should return 400 when date is in the past...")
+         logRunning()
 
         val requestBody = """
             {
@@ -322,7 +419,7 @@ class BookingControllerTest {
                 .content(requestBody)
         ).andExpect(status().isBadRequest)
 
-        LOGGER.info("test create booking should return 400 when date is in the past PASSED!")
+        logPassed()
     }
 
     @Test
@@ -330,7 +427,7 @@ class BookingControllerTest {
     @DisplayName("Test POST /api/bookings should return 404 when provided service point not found")
     @SqlSetup
     fun `test create should return 404 when provided service point not found`() {
-        LOGGER.info("RUNNING test create booking should return 404 when provided service point not found...")
+        logRunning()
 
         val requestBody = """
             {
@@ -348,14 +445,14 @@ class BookingControllerTest {
                 .content(requestBody)
         ).andExpect(status().isNotFound)
 
-        LOGGER.info("test create booking should return 404 when provided service point not found PASSED!")
+        logPassed()
     }
 
     @Test
     @DisplayName("Test DELETE /api/bookings/{id} should delete booking when user is author")
     @SqlSetup
     fun `test delete should delete booking when user is author`() {
-        LOGGER.info("RUNNING test delete booking should delete booking...")
+        logRunning()
 
         setUserContext(userId = 1)
         val bookingId = 1
@@ -369,8 +466,7 @@ class BookingControllerTest {
         mockMvc.perform(get("/api/bookings/$bookingId"))
             .andExpect(status().isNotFound)
 
-
-        LOGGER.info("test delete booking should delete booking PASSED!")
+        logPassed()
     }
 
     @Test
@@ -378,7 +474,7 @@ class BookingControllerTest {
     @DisplayName("Test DELETE /api/bookings/{id} should delete booking when user is admin")
     @SqlSetup
     fun `test delete should delete booking when user is admin`() {
-        LOGGER.info("RUNNING test delete booking should delete booking when user is admin...")
+        logRunning()
 
         setAdminContext(userId = 3)
         val bookingId = 1
@@ -392,14 +488,14 @@ class BookingControllerTest {
         mockMvc.perform(get("/api/bookings/$bookingId"))
             .andExpect(status().isNotFound)
 
-        LOGGER.info("test delete booking should delete booking when user is admin PASSED!")
+        logPassed()
     }
 
     @Test
     @DisplayName("Test DELETE /api/bookings/{id} should return 404 when booking not found")
     @SqlSetup
     fun `test delete should return 404 when booking not found`() {
-        LOGGER.info("RUNNING test delete booking should return 404 when booking not found...")
+        logRunning()
 
         setUserContext(userId = 1)
         val bookingId = 100
@@ -407,14 +503,14 @@ class BookingControllerTest {
         mockMvc.perform(delete("/api/bookings/$bookingId"))
             .andExpect(status().isNotFound)
 
-        LOGGER.info("test delete booking should return 404 when booking not found PASSED!")
+        logPassed()
     }
 
     @Test
     @DisplayName("Test DELETE /api/bookings/{id} should return 403 when user is not author")
     @SqlSetup
     fun `test delete should return 403 when user is not author`() {
-        LOGGER.info("RUNNING test delete booking should return 403 when user is not author...")
+        logRunning()
 
         setUserContext(userId = 2)
         val bookingId = 1
@@ -422,14 +518,14 @@ class BookingControllerTest {
         mockMvc.perform(delete("/api/bookings/$bookingId"))
             .andExpect(status().isForbidden)
 
-        LOGGER.info("test delete booking should return 403 when user is not author PASSED!")
+        logPassed()
     }
 
     @Test
     @DisplayName("Test PATCH /api/bookings/{id} should update booking when user is author")
     @SqlSetup
     fun `test update should update booking when user is author`() {
-        LOGGER.info("RUNNING test update booking should update booking...")
+        logRunning()
 
         setUserContext(userId = 1)
         val bookingId = 1
@@ -456,14 +552,14 @@ class BookingControllerTest {
             .andExpect(jsonPath("$.employeeId").value(1))
             .andExpect(jsonPath("$.treatmentId").value(1))
 
-        LOGGER.info("test update booking should update booking PASSED!")
+        logPassed()
     }
 
     @Test
     @DisplayName("Test PATCH /api/bookings/{id} should update when user is admin")
     @SqlSetup
     fun `test update should update when user is admin`() {
-        LOGGER.info("RUNNING test update booking should update when user is admin...")
+        logRunning()
 
         setAdminContext(userId = 3)
         val bookingId = 1
@@ -490,14 +586,14 @@ class BookingControllerTest {
             .andExpect(jsonPath("$.employeeId").value(1))
             .andExpect(jsonPath("$.treatmentId").value(1))
 
-        LOGGER.info("test update booking should update when user is admin PASSED!")
+        logPassed()
     }
 
     @Test
     @DisplayName("Test PATCH /api/bookings/{id} should return 404 when booking not found")
     @SqlSetup
     fun `test update should return 404 when booking not found`() {
-        LOGGER.info("RUNNING test update booking should return 404 when booking not found...")
+        logRunning()
 
         setUserContext(userId = 1)
         val bookingId = 100
@@ -518,14 +614,14 @@ class BookingControllerTest {
                 .content(requestBody)
         ).andExpect(status().isNotFound)
 
-        LOGGER.info("test update booking should return 404 when booking not found PASSED!")
+        logPassed()
     }
 
     @Test
     @DisplayName("Test PATCH /api/bookings/{id} should return 403 when user is not author")
     @SqlSetup
     fun `test update should return 403 when user is not author`() {
-        LOGGER.info("RUNNING test update booking should return 403 when user is not author...")
+        logRunning()
 
         setUserContext(userId = 2)
         val bookingId = 1
@@ -546,14 +642,14 @@ class BookingControllerTest {
                 .content(requestBody)
         ).andExpect(status().isForbidden)
 
-        LOGGER.info("test update booking should return 403 when user is not author PASSED!")
+        logPassed()
     }
 
     @Test
     @DisplayName("Test PATCH /api/bookings/{id} should return 400 when date is in the past")
     @SqlSetup
     fun `test update should return 400 when date is in the past`() {
-        LOGGER.info("RUNNING test update booking should return 400 when date is in the past...")
+        logRunning()
 
         setUserContext(userId = 1)
         val bookingId = 1
@@ -574,14 +670,14 @@ class BookingControllerTest {
                 .content(requestBody)
         ).andExpect(status().isBadRequest)
 
-        LOGGER.info("test update booking should return 400 when date is in the past PASSED!")
+        logPassed()
     }
 
     @Test
     @DisplayName("Test PATCH /api/bookings/{id} should ignore illegal field")
     @SqlSetup
     fun `test update should ignore illegal field`() {
-        LOGGER.info("RUNNING test update booking should ignore illegal field...")
+        logRunning()
 
         setUserContext(userId = 1)
         val bookingId = 1
@@ -606,7 +702,7 @@ class BookingControllerTest {
             .andExpect(jsonPath("$.employeeId").value(1))
             .andExpect(jsonPath("$.treatmentId").value(2))
 
-        LOGGER.info("test update booking should return smth when illegal field provided PASSED!")
+        logPassed()
     }
 
     private fun setUserContext(userId: Int) {
@@ -625,6 +721,14 @@ class BookingControllerTest {
             authorities = listOf(SimpleGrantedAuthority(role))
         )
         SecurityContextHolder.getContext().authentication = customAuth
+    }
+
+    private fun logRunning() {
+        LOGGER.info("RUNNING...")
+    }
+
+    private fun logPassed() {
+        LOGGER.info("PASSED!")
     }
 
     companion object {
